ac426b7a5b50f837cff449f5ed5a9ba6f95c4f10
NikhilPatel177 <nikhil.devspace@gmail.com>
2025-07-28 19:14:26 +0530

    production fixes

---
+++ server/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'], // main entry file
+  outDir: 'dist',
+  format: ['esm'], // or 'cjs' if you use CommonJS
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  target: 'es2022',
+  dts: false, // or true if you want .d.ts files
+});
