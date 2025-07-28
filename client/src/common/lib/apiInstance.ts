11481a2c9a20f2d0bc920d7d28d39ccccedd6569
NikhilPatel177 <nikhil.devspace@gmail.com>
2025-07-28 18:55:44 +0530

    minor change

--- client/src/common/lib/apiInstance.ts
+++ client/src/common/lib/apiInstance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 import { useAuthStore } from '../store/useAuthStore';
 
 const apiInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URI,
   withCredentials: true,
 });
 
