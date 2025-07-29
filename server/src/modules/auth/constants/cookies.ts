6749da609901c7cc1a196affc61b88549eec753d
NikhilPatel177 <nikhil.devspace@gmail.com>
2025-07-29 15:59:32 +0530

    production upgrades

--- server/src/modules/auth/constants/cookies.ts
+++ server/src/modules/auth/constants/cookies.ts
@@ -4,13 +4,13 @@ export const GOOGLE_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: env.NODE_ENV === 'production',
   maxAge: 15 * 60 * 1000, // 15 minutes
-  sameSite: 'lax' as const,
+  sameSite: 'none' as const,
 };
 
 export const REFRESHTOKEN_COOKIE_OPTIONS = {
   httpOnly: true,
   secure: env.NODE_ENV === 'production',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  sameSite: 'strict' as const,
+  sameSite: 'none' as const,
 };
 
